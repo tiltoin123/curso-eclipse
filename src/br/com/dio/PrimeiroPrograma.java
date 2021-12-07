43e71e305776475071c9e62b5a67841f9f49a140
Italo Eduardo Santos Souza <italohttp25@gmail.com>
2021-12-07 02:42:05 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.models.Gato;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+	Gato gato = new Gato(null, null, null);
+	
+	System.out.println(gato);
+	}
+
+}
